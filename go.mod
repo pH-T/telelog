747542453ac63fcc60490443ac6ba9191bd4dd8c
Paul Hager <paul.hager@foo.bar>
2020-07-14 19:34:46 +0200

    http client fix and removed timestamp

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/pH-T/telelog
 
-go 1.12
+go 1.14
 
-require github.com/pkg/errors v0.8.1
+require github.com/pkg/errors v0.9.1
--- telelog.go
+++ telelog.go
@@ -1,6 +1,9 @@
 package telelog
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"time"
@@ -27,55 +30,71 @@ func NewTelelog(token string, chatID string) *Telelog {
 }
 
 func sendMessage(msg string, token string, chatID string) error {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
 
 	data := url.Values{}
 	data.Set("chat_id", chatID)
 	data.Set("text", msg)
 
-	resp, err := http.PostForm(TELEGRAM_BOT_API+token+TELEGRAM_BOT_API_SEND_MESSAGE, data)
+	req, err := http.NewRequest("POST", TELEGRAM_BOT_API+token+TELEGRAM_BOT_API_SEND_MESSAGE, bytes.NewBuffer([]byte(data.Encode())))
+	if err != nil {
+		return errors.Wrap(err, "Could not create request")
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return errors.Wrap(err, "Could not POST data")
 	}
+	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return errors.Wrap(err, "Could not POST data: Status was not 200")
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Wrap(err, "Could not read response")
+	}
+
+	type telegramResponse struct {
+		Ok          bool   `json:"ok"`
+		ErrorCode   int    `json:"error_code"`
+		Description string `json:"description"`
+	}
+	res := telegramResponse{}
+	err = json.Unmarshal(bodyBytes, &res)
+	if err != nil {
+		return errors.Wrap(err, "Could not read json response into struct")
+	}
+
+	if resp.StatusCode != http.StatusOK || !res.Ok {
+		return errors.Wrap(err, "Could not POST data: HTTP-status was not 200 or status was not ok")
 	}
 
 	return nil
-
 }
 
+// Log logs the given msg with the given level (e.g. see 'TELELOG_LEVEL_ERROR' above) to telegram
 func (telelog *Telelog) Log(msg string, level string) error {
-
-	err := sendMessage(level+": "+time.Now().Format(time.RFC3339)+":\n "+msg, telelog.token, telelog.chatID)
+	err := sendMessage(level+":\n"+msg, telelog.token, telelog.chatID)
 	if err != nil {
 		return errors.Wrap(err, "log failed")
 	}
 
 	return nil
-
 }
 
 func (telelog *Telelog) LogError(msg string) error {
-
 	return telelog.Log(msg, TELELOG_LEVEL_ERROR)
-
 }
 
 func (telelog *Telelog) LogAlert(msg string) error {
-
 	return telelog.Log(msg, TELELOG_LEVEL_ALERT)
-
 }
 
 func (telelog *Telelog) LogInfo(msg string) error {
-
 	return telelog.Log(msg, TELELOG_LEVEL_INFO)
-
 }
 
 func (telelog *Telelog) LogSuccess(msg string) error {
-
 	return telelog.Log(msg, TELELOG_LEVEL_SUCCESS)
-
 }
